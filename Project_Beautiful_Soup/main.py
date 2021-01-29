07ed352f73e01665de6ff2d4edebe5cf0a0a88e4
sadhikarapu <76148434+sadhikarapu@users.noreply.github.com>
2021-01-28 21:28:39 -0800

    Webscrapping using BeautifulSoup

--- Project_Beautiful_Soup/main.py
+++ Project_Beautiful_Soup/main.py
@@ -1,4 +1,16 @@
 from bs4 import BeautifulSoup
 import requests
 
+headers = {'User-Agent':"Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:84.0) Gecko/20100101 Firefox/84.0"}
+http_browser = requests.get("https://www.moneycontrol.com/india/stockpricequote/banks-private-sector/yesbank/YB",headers = headers)
+
+soup = BeautifulSoup(http_browser.content,"lxml")
+
+get_title_text = soup.find_all("title") 
+
+
+data_split =  get_title_text[0].text.split(",")
+
+print(data_split[5].split(".")[0])
+
 
